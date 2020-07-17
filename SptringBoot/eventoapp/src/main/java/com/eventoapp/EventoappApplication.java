1bba99f38b73ad0e74b6ea9139f44d1dcff2301b
João Victor <joaosaraiva97@hotmail.com>
2020-07-17 16:44:01 -0300

    aula3

--- SptringBoot/eventoapp/src/main/java/com/eventoapp/EventoappApplication.java
+++ SptringBoot/eventoapp/src/main/java/com/eventoapp/EventoappApplication.java
@@ -1,10 +1,12 @@
-package com.eventoapp.eventoapp;
+package com.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan({"com.eventoapp.controllers"})
+@EntityScan(basePackages={"com.eventoapp.models"})
 public class EventoappApplication {
 
 	public static void main(String[] args) {
