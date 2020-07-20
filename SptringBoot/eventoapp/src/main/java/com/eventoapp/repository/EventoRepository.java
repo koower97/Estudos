26ec47b989abb0e682b811823ea1aaf42325bedc
João Victor <joaosaraiva97@hotmail.com>
2020-07-20 06:14:43 -0300

    aula8

--- SptringBoot/eventoapp/src/main/java/com/eventoapp/repository/EventoRepository.java
+++ SptringBoot/eventoapp/src/main/java/com/eventoapp/repository/EventoRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.eventoapp.models.Evento;
 
 public interface EventoRepository extends CrudRepository<Evento, String>{
-
-	
+	Evento findByCodigo(long codigo);
 }
