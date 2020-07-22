145542b084a4092df48c83edaf2f14225f552355
João Victor <joaosaraiva97@hotmail.com>
2020-07-22 05:21:05 -0300

    feito a validação de campos vazios

--- SptringBoot/eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ SptringBoot/eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,11 +1,15 @@
 package com.eventoapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eventoapp.models.Convidado;
 import com.eventoapp.models.Evento;
@@ -47,13 +51,21 @@ public class EventoController {
     	Evento evento = er.findByCodigo(codigo);
     	ModelAndView mv = new ModelAndView("evento/detalhesEvento");
     	mv.addObject("evento", evento);
+    	Iterable<Convidado> convidados = cr.findByEvento(evento);
+    	mv.addObject("convidados", convidados);
     	return mv;
     }
     @RequestMapping(value="/{codigo}", method=RequestMethod.POST)
-    public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado) {
+    public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado, BindingResult result, RedirectAttributes attributes) {
+    	if (result.hasErrors()) {
+    		attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+    		return "redirect:/{codigo}";
+    	}else {
     	Evento evento = er.findByCodigo(codigo);
     	convidado.setEvento(evento);
     	cr.save(convidado);
+    	attributes.addFlashAttribute("mensagem", "Convidado Adicionado Com Sucesso!");
     	return "redirect:/{codigo}";
+    	}
     }
 }
