6bb52156c3a1b20fa42c0662dbf2c19781f65d4d
João Victor <joaosaraiva97@hotmail.com>
2020-07-23 05:55:15 -0300

    Ajuste do design com CSS

--- SptringBoot/eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ SptringBoot/eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -31,10 +31,13 @@ public class EventoController {
     }
 
     @RequestMapping(value = "/cadastrarEvento", method=RequestMethod.POST)
-    public String form(Evento evento) {
-
+    public String form(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
+    	if(result.hasErrors()) {
+    		attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+    		return "redirect:/cadastrarEvento";
+    	}
         er.save(evento);
-
+        attributes.addFlashAttribute("mensagem", "Evento cadastrado com sucesso!");
         return "redirect:/cadastrarEvento";
     }
     
